bac984e34f16f28e6fcc07c0d99805dbfd5a638f
Kumaizasa <hanbe612@gmail.com>
2024-11-05 02:28:06 +0900

    Battle tmp & Effect base

--- Assets/Scripts/PlayerDataHolder.cs
+++ Assets/Scripts/PlayerDataHolder.cs
@@ -1,15 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using UnityEngine;
 
+//実際にステータスを管理するクラス　DDOS想定
 public class PlayerDataHolder : MonoBehaviour
 {
     public ParameterController.Player player;
+    public ParameterController.Weapon player_weapon;
+
+    [SerializeField] GameObject WeaponObj; //いつかリスト化する
+    public GameObject WeaponPrefab; //画面上での武器
+
     // Start is called before the first frame update
     void Start()
     {
         player = new ParameterController.Player("player", 1, 50, 10);
-        //UnityEngine.Debug.Log("name:" + player.name + " " + "Lv:" + player.Lv + " " + "HP/ATK:" + player.HP + "/" + player.ATK);
+        
+        player_weapon = new ParameterController.Weapon("tmp_sword", 10, ParameterController.SKILL.MAGIC);
+        WeaponPrefab = Instantiate(WeaponObj, new Vector3(0, 0, 0), Quaternion.identity);
+        UnityEngine.Debug.Log(player_weapon.GetName());
+        
     }
 
     // Update is called once per frame
