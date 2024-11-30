26bee97e43d413350cb63766d0cf9f8f73f68cb1
Kumaizasa <hanbe612@gmail.com>
2024-11-30 23:35:28 +0900

    Save temp version.

---
+++ Assets/Scripts/DropRenewer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// 新しい武器に更新するかどうかを制御するスクリプト
+public class DropRenewer : MonoBehaviour
+{
+    PlayerDataHolder playerdataholder;
+
+    [SerializeField] GameObject CurrentWeaponPrefab;
+    [SerializeField] GameObject NewWeaponPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerdataholder = GameManager.Instance.GetComponent<PlayerDataHolder>();
+
+        CurrentWeaponPrefab = playerdataholder.WeaponPrefab;
+        NewWeaponPrefab = playerdataholder.NewWeaponPrefab;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            playerdataholder.player_weapon = playerdataholder.new_weapon;
+            playerdataholder.WeaponPrefab = NewWeaponPrefab;
+            Destroy(CurrentWeaponPrefab);
+            SceneManager.LoadScene("Maze");
+        }
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            Destroy(NewWeaponPrefab);
+            SceneManager.LoadScene("Maze");
+        }
+    }
+}
--- Assets/Scripts/PlayerDataHolder.cs
+++ Assets/Scripts/PlayerDataHolder.cs
@@ -12,13 +12,24 @@ using static System.Net.Mime.MediaTypeNames;
 // 実際にステータスを管理するクラス
 public class PlayerDataHolder : MonoBehaviour
 {
-    // プレイヤーのデータと武器を初期化
+    // プレイヤーのデータと武器
     public ParameterController.Player player;
     public ParameterController.Weapon player_weapon;
 
-    // 画面上で武器を描画するための変数
-    public GameObject WeaponObj; //武器のパターンのリスト(予定)
+    // 画面上の武器
+    public GameObject WeaponObj; //武器の候補リスト(予定)
     public GameObject WeaponPrefab; // 画面上での武器
+
+    // ドロップ用
+    public ParameterController.Weapon new_weapon;
+    public GameObject NewWeaponPrefab;
+    private Renderer NewWeaponColor;
+
+    [SerializeField] GameObject DropPanel;
+    [SerializeField] GameObject OldDropText;
+    private UnityEngine.UI.Text olddroptext;
+    [SerializeField] GameObject NewDropText;
+    private UnityEngine.UI.Text newdroptext;
 
     // UI用パネルとテキストの準備
     [SerializeField] GameObject StatusPanel;
@@ -27,6 +38,13 @@ public class PlayerDataHolder : MonoBehaviour
 
     [SerializeField] GameObject BattlePanel;
     public GameObject BattleText;
+    private UnityEngine.UI.Text battletext;
+
+    public GameObject NumText_E;
+    private UnityEngine.UI.Text numtext_E;
+
+    public GameObject NumText_P;
+    private UnityEngine.UI.Text numtext_P;
 
 
     // Start is called before the first frame update
@@ -38,6 +56,9 @@ public class PlayerDataHolder : MonoBehaviour
         //武器初期宣言(名前、攻撃力、スキル)
         player_weapon = new ParameterController.Weapon("tmp_sword", 10, ParameterController.SKILL.MAGIC);
         WeaponPrefab = Instantiate(WeaponObj, new Vector3(0, 0, 0), Quaternion.identity);
+        WeaponPrefab.transform.SetParent(StatusPanel.transform, false);
+        WeaponPrefab.transform.localScale = new Vector3(250f, 250f, 250f);
+        UnityEngine.Debug.Log("Start Done");
 
     }
 
@@ -47,8 +68,9 @@ public class PlayerDataHolder : MonoBehaviour
         //ステータステキストを表示
         statustext = StatusText.GetComponent<UnityEngine.UI.Text>();
         statustext.text = "Name:" + player.Getname() + "  Lv:" + player.GetLv() + "\n"
-                           + "HP:" + player.GetHP() + "  ATK:" + player.GetATK() + "\n"
-                           + "Weapon:" + player_weapon.GetName();
+                           + "HP:" + player.GetHP() + "/" + player.GetMaxHP() + "  ATK:" + player.GetATK() + "\n"
+                           + "Weapon:" + player_weapon.GetWeaponName();
+
     }
 
     private void OnEnable()
@@ -77,6 +99,10 @@ public class PlayerDataHolder : MonoBehaviour
         {
             BattleSceneInit();
         }
+        else if (scene.name == "Drop")
+        {
+            DropSceneInit();
+        }
     }
 
     private IEnumerator InitializeMazeScene()
@@ -88,7 +114,14 @@ public class PlayerDataHolder : MonoBehaviour
     private IEnumerator InitializeBattleScene()
     {
         yield return null; // シーンロード後1フレーム待つ
+        //yield return new WaitForSeconds(3f); // シーンロード後3秒待つ
         BattleSceneInit();
+    }
+
+    private IEnumerator InitializeDropScene()
+    {
+        yield return null; // シーンロード後1フレーム待つ
+        DropSceneInit();
     }
 
     // Mazeシーン読み込み時にUI系オブジェクトの位置を調整
@@ -96,12 +129,11 @@ public class PlayerDataHolder : MonoBehaviour
     {
         UnityEngine.Debug.Log("迷路用の初期化処理を実行します。");
         // プレイヤーの武器をMaze UIに配置
-        WeaponPrefab.transform.SetParent(StatusPanel.transform, false);
         WeaponPrefab.transform.localPosition = new Vector3(0, -30f, -1f);
-        WeaponPrefab.transform.localScale = new Vector3(250f, 250f, 250f);
-        WeaponPrefab.transform.Rotate(0, -180f, 0);
+        //WeaponPrefab.transform.Rotate(0, -180f, 0);
 
         BattlePanel.SetActive(false);
+        DropPanel.SetActive(false);
     }
 
     private void BattleSceneInit()
@@ -112,6 +144,35 @@ public class PlayerDataHolder : MonoBehaviour
         WeaponPrefab.transform.Rotate(0, 180f, 0);
 
         BattlePanel.SetActive(true);
+        DropPanel.SetActive(false);
 
+    }
+
+    private void DropSceneInit()
+    {
+        UnityEngine.Debug.Log("ドロップ用の初期化処理を実行します。");
+        DropPanel.SetActive(true);
+
+        // 新しい武器を生成
+        new_weapon = new ParameterController.Weapon("new_sword", 15, ParameterController.SKILL.HEAL);
+        NewWeaponPrefab = Instantiate(WeaponObj, new Vector3(0, 0, 0), Quaternion.identity);
+
+        NewWeaponPrefab.transform.SetParent(StatusPanel.transform, false);
+        NewWeaponPrefab.transform.localScale = new Vector3(250f, 250f, 250f);
+        NewWeaponPrefab.transform.position = new Vector3(4f, 1f, 0);
+        NewWeaponColor = NewWeaponPrefab.GetComponent<Renderer>();
+        NewWeaponColor.material.color = Color.red;
+
+        newdroptext = NewDropText.GetComponent<UnityEngine.UI.Text>();
+        newdroptext.text = "名前：" + new_weapon.GetWeaponName() + "\n" + "ATK:" + new_weapon.GetWeaponATK() + "\n" + "SKILL:" + new_weapon.GetWeaponSKILL();
+
+        //プレイヤーの武器を取得して位置を調整
+        WeaponPrefab.transform.position = new Vector3(-4f, 1f, 0);
+
+        olddroptext = OldDropText.GetComponent<UnityEngine.UI.Text>();
+        olddroptext.text = "名前：" + player_weapon.GetWeaponName() + "\n" + "ATK:" + player_weapon.GetWeaponATK() + "\n" + "SKILL:" + player_weapon.GetWeaponSKILL();
+
+        battletext = BattleText.GetComponent<UnityEngine.UI.Text>();
+        battletext.text = "新しい武器を入手しました！ 武器を変えますか？" + "         " + "Y/N";
     }
 }
